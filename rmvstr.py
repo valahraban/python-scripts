8f582bb54217e43dedba2c9df687e4f6b6ae30b5
valahraban <76002154+valahraban@users.noreply.github.com>
2021-06-08 22:20:33 +0300

    Update rmvstr.py

--- rmvstr.py
+++ rmvstr.py
@@ -8,4 +8,5 @@ source_file.close()
 new_file = open(input("Enter a filename to output: "), "w", encoding="utf-8")
 for line in lines:
     line.replace("=","") ##add one of these for every chr/str you want to remove
+    new_file.write(line)
 new_file.close()
