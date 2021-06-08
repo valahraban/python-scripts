9845b4855f1fc3a1f44999345f7cf526354f5560
valahraban <76002154+valahraban@users.noreply.github.com>
2021-06-08 22:43:44 +0300

    Update rmvstr.py

--- rmvstr.py
+++ rmvstr.py
@@ -1,12 +1,11 @@
-## A script you define a list of bad characters or strings in
-## If your .txt file contains said strings they get removed while everything else stays the same
-## Warning, I wrote this in github without any testing, write to a new filename if you fear bricking your source
+## A script for replacing strings of any size
+## It works now, just don't try to get too clever, the placeholder variable in for loop is required
 ## WTFPL by valahraban, rmvstr=removestring
 source_file = open(input("Enter a filename to modify: "), "r")
 lines = source_file.readlines()
 source_file.close()
 new_file = open(input("Enter a filename to output: "), "w", encoding="utf-8")
-for line in lines:
-    line.replace("=","") ##add one of these for every chr/str you want to remove
-    new_file.write(line)
+for line in lines: ##repeat with new strings for every string you want replaced, replacer can replace itsel
+    replacer = line.replace("=","") 
+    new_file.write(replacer)
 new_file.close()
