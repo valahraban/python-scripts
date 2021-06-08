e21481128937a26e070e0a47da8763c18f3c2e2c
valahraban <76002154+valahraban@users.noreply.github.com>
2021-06-08 22:43:55 +0300

    Update rmvstr.py

--- rmvstr.py
+++ rmvstr.py
@@ -5,7 +5,7 @@ source_file = open(input("Enter a filename to modify: "), "r")
 lines = source_file.readlines()
 source_file.close()
 new_file = open(input("Enter a filename to output: "), "w", encoding="utf-8")
-for line in lines: ##repeat with new strings for every string you want replaced, replacer can replace itsel
+for line in lines: ##repeat with new strings for every string you want replaced, replacer can replace itself
     replacer = line.replace("=","") 
     new_file.write(replacer)
 new_file.close()
