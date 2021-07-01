aac5f99403bb7d21e0b1da2d75f0b659e9161934
valahraban <76002154+valahraban@users.noreply.github.com>
2021-07-01 18:00:15 +0300

    Update globtext.py

--- globtext.py
+++ globtext.py
@@ -1,17 +1,18 @@
 ## Run this script in folder
-## Script goes through .txt files located with glob and performs modifications on the entire text
+## Script goes through .txt files located with glob and performs modifications on the entire file
 ## The defaults of this program are for processing .txt for use in NLP datasetting
-## WTFPL by valahraban
+## For use in your own batch text editing, learn to use regex and tweak the script for your needs
+## Licensed: GPLv3.0 by valahraban
 import sys
 import re ## regex ## re.I, re.M
 from re import search ## regex search if needed 
 import glob
 import ftfy
 
-#for filename in glob.glob("**/*.txt", recursive=True): ## if you want to do recursive mode instead which is more dangerous
-for filename in glob.glob("*.txt": ## glob stores filenames
-    with open(filename, 'r+', encoding='utf-8') as f: ## open files in rw-mode, iterate
-        text = f.read() ## modification go under this
+for filename in glob.glob("**/*.txt", recursive=True): ## if you want to do recursive mode instead which is more dangerous
+#for filename in glob.glob("*.txt"): ## glob stores filenames
+    with open(filename, 'r+', encoding='utf-8', errors='ignore') as f: # open files in rw-mode, iterate, skip encoding errors
+        text = f.read() # modification go under this
         text = re.sub(r'( |\t)+', ' ', text) # unindent
         text = re.sub(r'(^|\n)( |\t)+', r'\1', text) # unindent
         text = ftfy.fix_text(text).replace(' …', '...').replace('…', '...').replace('\N{SOFT HYPHEN}', '').replace('\u200b', '').replace(u"\uFFFD", '') # clean up special
@@ -26,10 +27,14 @@ for filename in glob.glob("*.txt": ## glob stores filenames
         text = re.sub(r'\bwww\.[a-zA-Z0-9\-\.\/\~\_]+', '', text) # purging www before doing newlines
         text = text.replace('\r\n', '\n').replace('\r', '\n') # normalize newlines
         text = text.replace('\n\n\n', '\n').replace('\n\n', '\n') # no linebreaks
-        text = re.sub(r'^(epilogue.?|prologue.?|afterword.?)$', "***", text, flags=re.I | re.M)
-        text = re.sub('^(begin reading|insert|table of contents|title page|copyright page|dedication.?|acknowledgement.?|content.?|endnote.?)$', "", text, flags=re.I | re.M)
+        text = re.sub(r'^(epilogue.?|prologue.?)(\n|$)', "***", text, flags=re.I | re.M)
+        text = re.sub('^(begin reading|insert|table of contents|title page|copyright page|dedication.?|acknowledgement.?|content.?|endnote.?)(\n|$)', "", text, flags=re.I | re.M)
+        text = re.sub(r'<\|endoftext\|>', '', text)
+        text = re.sub(r'^(.*)?(Yen On|Yen Press|Tor Publishing)(.*)?(\n|$)', "", text, flags=re.M) # purge undesired strings
+        text = re.sub(r'^Identifiers:?(.*)?(\n|$)', "", text, flags=re.M)
+        text = re.sub(r'^ISBN(.*)?(\n|$)', "", text, flags=re.M)
         f.seek(0) ## pointer at top of file
         f.write(text) ## write to file
-        f.truncate() ## truncate and close, continues
+        f.truncate() ## truncate and close, continue
 
-#input("press return to exit")
+input("press return to exit") # optional
