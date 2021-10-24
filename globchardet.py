377bf063ead2c74a6e4a001ce133369796600c32
valahraban <76002154+valahraban@users.noreply.github.com>
2021-10-24 14:53:25 +0300

    Update globchardet.py

--- globchardet.py
+++ globchardet.py
@@ -1,7 +1,6 @@
 ## really hacky python script to estimate the encoding of all the files in your folder with chardet and a tqdm loadbar: run at your own risk
 ## meant to be used while piping the output to a text file from your shell if you're working on a massive amount of files e.g.: python globchardet.py >> output.txt
 ## WTFPL by valahraban
-
 import sys
 import glob
 import chardet
